893d2607d363a9438a030b0c46173f27b0014ae3
vibhortaneja <vtaneja127@gmail.com>
2019-02-19 03:47:17 +0530

    First Commit

---
+++ src/main/java/com/authservice/niit/jwtsecurity/contoller/TokenController.java
@@ -0,0 +1,28 @@
+package com.authservice.niit.jwtsecurity.contoller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.authservice.niit.jwtsecurity.model.JwtUser;
+import com.authservice.niit.jwtsecurity.security.JwtGenerator;
+
+@RestController
+@RequestMapping("/token")
+public class TokenController {
+
+
+    private JwtGenerator jwtGenerator;
+
+    public TokenController(JwtGenerator jwtGenerator) {
+        this.jwtGenerator = jwtGenerator;
+    }
+
+    @PostMapping
+    public String generate(@RequestBody final JwtUser jwtUser) {
+
+        return jwtGenerator.generate(jwtUser);
+
+    }
+}
---
+++ src/main/java/com/authservice/niit/jwtsecurity/security/JwtAuthenticationProvider.java
@@ -0,0 +1,40 @@
+package com.authservice.niit.jwtsecurity.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthenticationProvider extends AbstractUserDetailsAuthenticationProvider {
+
+    @Autowired
+    private JwtValidator validator;
+
+    @Override
+    protected void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken) throws AuthenticationException {
+
+    }
+
+    @Override
+    protected UserDetails retrieveUser(String username, UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken) throws AuthenticationException {
+
+        JwtAuthenticationToken jwtAuthenticationToken = (JwtAuthenticationToken) usernamePasswordAuthenticationToken;
+        String token = jwtAuthenticationToken.getToken();
+
+        JwtUser jwtUser = validator.validate(token);
+
+        if (jwtUser == null) {
+            throw new RuntimeException("JWT Token is incorrect");
+        }
+
+        List<GrantedAuthority> grantedAuthorities = AuthorityUtils
+                .commaSeparatedStringToAuthorityList(jwtUser.getRole());
+        return new JwtUserDetails(jwtUser.getUserName(), jwtUser.getId(),
+                token,
+                grantedAuthorities);
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return (JwtAuthenticationToken.class.isAssignableFrom(aClass));
+    }
+}
